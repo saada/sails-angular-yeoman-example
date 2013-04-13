6a3db5d2a379f04b688b8d9008509262ae994efd
Mahmoud Saada <mahmoudsaada@gmail.com>
2013-04-13 15:50:03 -0700

    small fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 1337,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost'
       },
