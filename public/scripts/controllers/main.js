bf44ecd0ddf0069788fc42161a322c953f6182be
Mahmoud Saada <mahmoudsaada@gmail.com>
2013-04-14 14:32:21 -0700

    update

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -3,11 +3,27 @@ angular.module('YoSailsAngularApp')
 	.controller('MainCtrl', [
 		'$scope','$resource',
 		function ($scope, $resource) {
-			$scope.users = $resource('users').query();
 			$scope.awesomeThings = [
 				'HTML5 Boilerplate',
 				'AngularJS',
 				'Karma'
 			];
+
+			// RESTful stuff
+			var users = $resource('users');
+			$scope.users = users.query();
+
+			$scope.add = function(user){
+				if(typeof user.count === 'undefined'){
+					user.count = 0;
+				}
+				else{
+					user.count++;
+				}
+				var restUser = users.get({id:user.id},function(){
+					restUser.count = user.count;
+					restUser.save();
+				});
+			};
 		}
 	]);
